2ca9ca879d6067689856436202021f1d06ecc6e3
yongbingao <ben.gao25@gmail.com>
2019-04-12 14:54:03 -0400

    Add valid_moves method

--- w1d5/kpf.rb
+++ w1d5/kpf.rb
@@ -1,3 +1,5 @@
+require_relative "skeleton/lib/00_tree_node"
+
 class KnightPathFinder
     def initialize(start_pos)
         @start_pos = start_pos
@@ -16,6 +18,21 @@ class KnightPathFinder
     end
 
     def self.valid_moves(pos)
+        left = pos.first
+        right = pos.last
+        valid_pos = []
+
+        (left-2..left+2).each do |row_idx|
+            (right-2..right+2).each do |col_idx|
+                unless row_idx > 7 || row_idx < 0 || col_idx > 7 || col_idx < 0
+                    if ((left - row_idx).abs == 1 && (right - col_idx).abs == 2) || 
+                        ((left - row_idx).abs == 2 && (right - col_idx).abs == 1)
+                        valid_pos << [row_idx, col_idx]
+                    end
+                end
+            end
+        end
+        valid_pos                             
     end
 
 end
