23701938d7bba2c4e4fb6adcdb67f878c3c3de4a
bhanu-gupta <guptabhanu90@gmail.com>
2019-04-12 14:02:27 -0400

    add polytee node

---
+++ w1d5/skeleton/lib/00_tree_node.rb
@@ -0,0 +1,58 @@
+class PolyTreeNode
+    attr_reader :parent, :children, :value
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(parent_node) #parent_node = node 1 @parent = nil @children =[]
+        if !self.parent.nil?
+            self.parent.children.delete(self)
+        end
+        @parent = parent_node #node 2 @parent = node 1 @children = []
+        if !parent_node.nil? && !parent_node.children.include?(self)
+            parent_node.children << self
+        end
+    end
+
+    def add_child(child_node)
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        self.children.include?(child_node) ? child_node.parent = nil : (raise "Child not present")
+    end
+
+    def inspect
+        @value.inspect
+        @parent.inspect
+        @children.inspect
+    end
+
+    def dfs(target_value)
+        return nil if self == nil
+        return self if self.value == target_value
+        self.children.each do |child|
+            result = child.dfs(target_value)
+            return result if result != nil
+        end
+        nil
+    end
+
+    def bfs(target_value)
+        queue = []
+        queue << self
+        until queue.empty?
+            first_ele = queue.shift
+            if target_value == first_ele.value
+                return first_ele
+            else
+                queue += first_ele.children
+            end
+        end
+        nil
+    end
+
+end
